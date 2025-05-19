4407b7f6ce4b98b25ffd6da1d84805ed1497dd02
Guilherme Gomes <guilherme.gomes@v360.io>
2025-05-19 11:23:13 -0300

    fix: tests #3

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,23 +1,32 @@
 require "test_helper"
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get items_create_url
-    assert_response :success
+  setup do
+    @todo_list = todo_lists(:one)
+    @item = items(:one)
   end
 
-  test "should get destroy" do
-    get items_destroy_url
-    assert_response :success
+  test "should create item" do
+    assert_difference("Item.count") do
+      post todo_list_items_url(@todo_list), params: { item: { description: "Novo item" } }
+    end
+    assert_redirected_to todo_list_url(@todo_list)
   end
 
-  test "should get update" do
-    get items_update_url
-    assert_response :success
+  test "should destroy item" do
+    assert_difference("Item.count", -1) do
+      delete todo_list_item_url(@todo_list, @item)
+    end
+    assert_redirected_to todo_list_url(@todo_list)
   end
 
-  test "should get toggle_complete" do
-    get items_toggle_complete_url
-    assert_response :success
+  test "should update item" do
+    patch todo_list_item_url(@todo_list, @item), params: { item: { description: "Atualizado" } }
+    assert_redirected_to todo_list_url(@todo_list)
+  end
+
+  test "should toggle complete item" do
+    patch toggle_complete_todo_list_item_url(@todo_list, @item)
+    assert_redirected_to todo_list_url(@todo_list)
   end
 end
--- test/controllers/todo_lists_controller_test.rb
+++ test/controllers/todo_lists_controller_test.rb
@@ -2,37 +2,45 @@ require "test_helper"
 
 class TodoListsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get todo_lists_index_url
+    get todo_lists_url
     assert_response :success
   end
 
   test "should get new" do
-    get todo_lists_new_url
+    get new_todo_list_url
     assert_response :success
   end
 
-  test "should get create" do
-    get todo_lists_create_url
-    assert_response :success
+  test "should create todo_list" do
+    assert_difference("TodoList.count") do
+      post todo_lists_url, params: { todo_list: { title: "Nova Lista", description: "Descrição" } }
+    end
+    assert_redirected_to todo_list_url(TodoList.last)
   end
 
   test "should get edit" do
-    get todo_lists_edit_url
+    todo_list = todo_lists(:one)
+    get edit_todo_list_url(todo_list)
     assert_response :success
   end
 
-  test "should get update" do
-    get todo_lists_update_url
-    assert_response :success
+  test "should update todo_list" do
+    todo_list = todo_lists(:one)
+    patch todo_list_url(todo_list), params: { todo_list: { title: "Atualizado" } }
+    assert_redirected_to todo_list_url(todo_list)
   end
 
-  test "should get destroy" do
-    get todo_lists_destroy_url
-    assert_response :success
+  test "should destroy todo_list" do
+    todo_list = todo_lists(:one)
+    assert_difference("TodoList.count", -1) do
+      delete todo_list_url(todo_list)
+    end
+    assert_redirected_to todo_lists_url
   end
 
-  test "should get show" do
-    get todo_lists_show_url
+  test "should show todo_list" do
+    todo_list = todo_lists(:one)
+    get todo_list_url(todo_list)
     assert_response :success
   end
 end
